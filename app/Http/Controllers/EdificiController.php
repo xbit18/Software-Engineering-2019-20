c16bbf250cf1b800aaef0b6ff091f3a32961a517
Giacomo Sfratato <giacomo.sfratato@student.univaq.it>
2020-01-14 15:05:11 +0100

    update

--- app/Http/Controllers/EdificiController.php
+++ app/Http/Controllers/EdificiController.php
@@ -21,16 +21,6 @@ class EdificiController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        return view('edifici.create');
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
