dc44704e9003175c12ed794934c898187890826e
Dawid Pado <dawidpado97@gmail.com>
2020-01-17 14:30:02 +0100

    adding JWT Auth

--- app/Utente.php
+++ app/Utente.php
@@ -3,8 +3,9 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class Utente extends Model
+class Utente extends Model implements JWTSubject
 {
     protected $table = 'utenti';
 
@@ -18,5 +19,27 @@ class Utente extends Model
 
     public function prenotazioni(){
         return $this->hasMany('App\Prenotazione');
+    }
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+        // TODO: Implement getJWTIdentifier() method.
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+        // TODO: Implement getJWTCustomClaims() method.
     }
 }
