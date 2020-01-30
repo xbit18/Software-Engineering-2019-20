fc055559ea621dbb21c0ae8fa1845187f9604698
Giacomo Sfratato <xbit18@hotmail.it>
2020-01-30 15:00:04 +0100

    Modifiche ad attendances

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,32 +1,28 @@
 <template>
-    <div id="app">
-        <Header></Header>
-        <router-view></router-view>
-        <FooterComponent></FooterComponent>
-    </div>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
-    import Header from './components/Header.vue';
-    import FooterComponent from './components/FooterComponent.vue';
-    export default {
-        name: 'app',
-        components: {
-            Header,
-            FooterComponent
-        }
-    }
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
 </script>
 
 <style>
-    /* #app {
-      font-family: 'Avenir', Helvetica, Arial, sans-serif;
-      -webkit-font-smoothing: antialiased;
-      -moz-osx-font-smoothing: grayscale;
-      text-align: center;
-      color: #2c3e50;
-      margin-top: 60px;
-    }*/
-
-    @import url('./assets/css/styles.css');
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
